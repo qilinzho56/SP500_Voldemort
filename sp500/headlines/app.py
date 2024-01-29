9b4366275ab045c7e0dda428122b56f2d46b91e3
Alexandria2109 <117550186+Alexandria2109@users.noreply.github.com>
2024-01-29 00:08:20 -0600

    App

---
+++ sp500/headlines/app.py
@@ -0,0 +1,9 @@
+from sp500.headlines.displayer import TickerApplication
+
+if __name__ == "__main__":
+    headers = {"User-Agent": "Mozilla/5.0",
+                    "Referer": "http://finviz.com/quote.ashx?t="}
+    display = TickerApplication()
+    display.run_window1(headers)
+    display.update_window2()
+    display.run_window2()
