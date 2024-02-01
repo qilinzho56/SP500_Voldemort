1f45fd821dbb03d4abd237ecc0f4411c41848046
WU BEICHI <wubeichi@MacBook-Pro-3.local>
2024-02-01 12:20:13 -0600

    version 0.1.2

---
+++ sp500/compile/pos.py
@@ -0,0 +1,66 @@
+#from sp500.headlines.scraper import headlines
+import pandas as pd
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def add_new_column(dict, df, row_index):
+    """
+    According to part of speech of words, we add them
+    to the dataframe
+
+    Parameters
+    ----------
+    dict: dict
+        A dictionary containing part of speech and corresponding words
+    df: dataframe
+        The dataframe we import
+    row_index: int
+        row index
+    """
+
+    for pos in dict:
+        if pos not in df.columns:
+            df[pos] = None
+        df.at[row_index, pos] = ', '.join(dict[pos])
+
+
+def POS_tagging(text, df, row_index):
+    """
+    Using spaCy to do Part of Speech (POS) Tagging
+    
+    Parameters
+    ----------
+    text: str
+        News headline we scrap
+    df: dataframe
+        The dataframe we import
+    row_index: int
+    """
+
+    part_of_speech_dict = {}
+    docs = nlp(text)
+    
+    for word in docs:
+        if word.pos_ not in part_of_speech_dict:
+            part_of_speech_dict[word.pos_] = []
+            part_of_speech_dict[word.pos_].append(word.text)
+        else:
+            part_of_speech_dict[word.pos_].append(word.text)
+    
+    add_new_column(part_of_speech_dict, df, row_index)
+
+
+def POS_in_DF(headers = None, company_list = None, max_days = None):
+
+    
+    # df = headlines(headers, company_list, max_days)
+    df = pd.read_csv("/Users/wubeichi/Desktop/Computer Science with Applications II/CS 2/SP500/Jan_24_Jan_28_Headline2.csv")
+    for index, row in df.iterrows():
+        POS_tagging(row["Headline"], df, index)
+    
+    df.to_csv("./SP500/Modified_Headlines.csv", index=False)
+    
+if __name__ == "__main__":
+    POS_in_DF()
