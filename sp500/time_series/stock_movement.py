e58e2a83529037fc0a9c7b68b1dbf23639f1f09a
Yolanda Y <144951320+Yolandaaay@users.noreply.github.com>
2024-03-02 15:52:07 -0600

    Update stock_movement.py

--- sp500/time_series/stock_movement.py
+++ sp500/time_series/stock_movement.py
@@ -1,9 +1,7 @@
+import plotly.graph_objects as go
 from sp500.time_series.time_series import load_ticker_data, ema_macd, obv
 import pandas as pd
-import mplfinance as mpf
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 
 def add_macd(df, window_signal=9):
@@ -16,68 +14,113 @@ def add_macd(df, window_signal=9):
     return df
 
 
-def plot_stock_data(company):
-    """
-    Plots real-time stock data for a given company using mplfinance
+def add_sma(df):
+    for window in [20, 50, 200]:
+        sma_column = f"SMA_{window}"
+        df[sma_column] = df["Adj Close"].rolling(window).mean()
+    return df
 
-    Parameters:
-    - company: (string) the ticker symbol of the company
-    """
+
+def plot_stock_data_interactive(company):
     ticker_df = load_ticker_data(company)
     ticker_df.index = pd.to_datetime(ticker_df.index)
-    start_date = datetime.now() - timedelta(days=126)
-    halfyear_data = ticker_df[ticker_df.index >= start_date]
+    start_date = datetime.now() - timedelta(days=400)
+    df = ticker_df[ticker_df.index >= start_date]
+    df_macd = ema_macd(df)
+    df_with_indicators = add_macd(df_macd)
+    df_with_indicators2 = obv(df)
+    df_with_indicators3 = add_sma(df)
 
-    df = ema_macd(halfyear_data)
-    macd = add_macd(df)
-    obv_data = obv(halfyear_data)
-    plots = [
-        mpf.make_addplot(
-            (macd["MACD"]), type="line", color="blue", panel=2, ylabel="MACD"
-        ),
-        mpf.make_addplot((macd["signal"]), type="line", color="grey", panel=2),
-        mpf.make_addplot((macd["bar_positive"]), type="bar", color="green", panel=2),
-        mpf.make_addplot((macd["bar_negative"]), type="bar", color="red", panel=2),
-        mpf.make_addplot(
-            (obv_data["OBV"]), type="line", color="pink", panel=3, ylabel="OBV"
-        ),
-    ]
-
-    plot_color = mpf.make_marketcolors(up="green", down="red", volume="inherit")
-    plot_style = mpf.make_mpf_style(marketcolors=plot_color)
-    plot_args = {
-        "figratio": (5, 2),
-        "panel_ratios": (5, 1, 2, 1),
-        "figscale": 1.5,
-        "type": "candle",
-        "mav": (3, 6, 9),
-        "volume": True,
-        "volume_panel": 1,
-        "addplot": plots,
-        "tight_layout": True,
-        "style": plot_style,
-    }
-
-    fig, axes = mpf.plot(halfyear_data, **plot_args, returnfig=True)
-
-    ax = axes[0]
-    red_patch = mpatches.Patch(color="red", label="Price Down")
-    green_patch = mpatches.Patch(color="green", label="Price Up")
-    ax.legend(handles=[green_patch, red_patch], loc="upper right")
-
-    latest_price = halfyear_data["Close"].iloc[-1]
-    title_text = (
-        f"Stock Movement for {company.upper()}\nLatest Price: ${latest_price:.2f}"
-    )
-    box_props = dict(boxstyle="square", facecolor="white", edgecolor="black", alpha=0.8)
-    fig.suptitle(
-        title_text,
-        fontsize=11,
-        fontweight="bold",
-        ha="left",
-        x=0.13,
-        y=0.95,
-        bbox=box_props,
+    candlestick = go.Candlestick(
+        x=df_with_indicators.index,
+        open=df_with_indicators["Open"],
+        high=df_with_indicators["High"],
+        low=df_with_indicators["Low"],
+        close=df_with_indicators["Close"],
+        name="Price",
     )
 
-    plt.show()
+    macd_line = go.Scatter(
+        x=df_with_indicators.index,
+        y=df_with_indicators["MACD"],
+        mode="lines",
+        name="MACD",
+        yaxis="y4",
+    )
+    signal_line = go.Scatter(
+        x=df_with_indicators.index,
+        y=df_with_indicators["signal"],
+        mode="lines",
+        name="Signal",
+        yaxis="y4",
+    )
+
+    obv_line = go.Scatter(
+        x=df_with_indicators2.index,
+        y=df_with_indicators2["OBV"],
+        mode="lines",
+        name="OBV",
+        yaxis="y2",
+    )
+
+    sma_20_line = go.Scatter(
+        x=df_with_indicators3.index,
+        y=df_with_indicators3["SMA_20"],
+        mode="lines",
+        name="SMA 20",
+        line=dict(color="blue", width=2),
+    )
+
+    sma_50_line = go.Scatter(
+        x=df_with_indicators3.index,
+        y=df_with_indicators3["SMA_50"],
+        mode="lines",
+        name="SMA 50",
+        line=dict(color="green", width=2),
+    )
+
+    sma_200_line = go.Scatter(
+        x=df_with_indicators3.index,
+        y=df_with_indicators3["SMA_200"],
+        mode="lines",
+        name="SMA 200",
+        line=dict(color="yellow", width=2),
+    )
+
+    volume_bars = go.Bar(
+        x=df.index,
+        y=df["Volume"],
+        name="Volume",
+        marker=dict(color="rgba(128, 128, 128, 0.5)"),
+        yaxis="y3",
+    )
+
+    layout = go.Layout(
+        title=f"Stock Movement for {company}",
+        xaxis=dict(
+            rangeslider=dict(visible=True), type="category", tickmode="auto", nticks=20
+        ),
+        yaxis=dict(title="Stock Price", domain=[0.2, 1]),
+        yaxis2=dict(
+            title="OBV", side="right", overlaying="y", position=0.95, domain=[0.1, 0.2]
+        ),
+        yaxis3=dict(
+            title="Volume", side="right", overlaying="y", position=0.97, domain=[0, 0.1]
+        ),
+        yaxis4=dict(title="MACD", side="right", overlaying="y", position=0.999),
+    )
+
+    fig = go.Figure(
+        data=[
+            candlestick,
+            macd_line,
+            signal_line,
+            obv_line,
+            volume_bars,
+            sma_20_line,
+            sma_50_line,
+            sma_200_line,
+        ],
+        layout=layout,
+    )
+    fig.show()
