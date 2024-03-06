7a2ff5c303d5a86bc147ce37e0265821b0b0726d
Beichi WU <wubeichi@MacBook-Pro-3.local>
2024-03-06 16:21:14 -0600

    update

--- sp500/sa/analyzer.py
+++ sp500/sa/analyzer.py
@@ -1,15 +1,22 @@
 from sp500.sa.sa import calculate_score
 import pathlib
+import pandas as pd
 
-def implementation(labeled_data_filename):
+def implementation(df = None):
     """
     Implement the sentiment analyzer.
     """
     # Import the file
-    calculate_score(labeled_data_filename)
+
+    if not df:
+        labeled_data_filename = (
+            pathlib.Path(__file__).parent / "data/Jan_24_Jan_28_Stock_News.csv"
+        )
+
+        labeled_data = pd.read_csv(labeled_data_filename, encoding="unicode_escape")
+        calculate_score(labeled_data)
+    else:
+        calculate_score(df)
 
 if __name__ == "__main__":
-    labeled_data_filename = (
-        pathlib.Path(__file__).parent / "data/New_data.csv.csv"
-    )
-    implementation(labeled_data_filename)
+    implementation()
